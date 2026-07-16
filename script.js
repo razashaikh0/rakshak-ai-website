5345e78fb6a785adaab21a85e317a7215085e02e
razashaikh0 <razashaikh78888@gmail.com>
2026-07-17 01:11:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const launch = new Date(
+    "August 1, 2026 17:00:00"
+).getTime();
 
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const gap = launch - now;
+
+    const days = Math.floor(
+        gap / (1000 * 60 * 60 * 24)
+    );
+
+    const hours = Math.floor(
+        (gap % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (gap % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (gap % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000);
